529b57bb5e0f6859f2c2dd0130de240a0e70d684
Syket Das <saiketsd23@gmail.com>
2021-08-12 19:57:58 +0600

    s

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-import Home from "./pages/Home";
-import Rooms from "./pages/Rooms";
-import SingleRoom from "./pages/SingleRoom";
-import Error from "./pages/Error";
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
 
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
 
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Navbar />
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route
+          exact
+          path="https://syket112.github.io/Resort-management-website-React-/"
+          component={Home}
+        />
         <Route exact path="/rooms/" component={Rooms} />
         <Route exact path="/rooms/:slug" component={SingleRoom} />
         <Route component={Error} />
