05a36696f2b6eb94b444a842e10133942bbb5619
Syket Das <saiketsd23@gmail.com>
2021-08-12 19:48:34 +0600

    Updates

---
+++ precache-manifest.45c901a5f89678d78f97241c6967ab96.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6234f69822eabac98fef3e54d68c891d",
+    "url": "/Resort-management-website-React-/index.html"
+  },
+  {
+    "revision": "c1e53caf63836da7ff71",
+    "url": "/Resort-management-website-React-/static/css/main.e606d2cd.chunk.css"
+  },
+  {
+    "revision": "bfe2d504264bf1129bd9",
+    "url": "/Resort-management-website-React-/static/js/2.1577a640.chunk.js"
+  },
+  {
+    "revision": "c1e53caf63836da7ff71",
+    "url": "/Resort-management-website-React-/static/js/main.c3e49f29.chunk.js"
+  },
+  {
+    "revision": "5f79043400ba9fe1de31",
+    "url": "/Resort-management-website-React-/static/js/runtime~main.1aab59f0.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/Resort-management-website-React-/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/Resort-management-website-React-/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "50c23e6265e40d602f1ac080e2d4169a",
+    "url": "/Resort-management-website-React-/static/media/details-2.50c23e62.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/Resort-management-website-React-/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/Resort-management-website-React-/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/Resort-management-website-React-/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "c7c76c7a194983fa489c0e1b2d48dd3b",
+    "url": "/Resort-management-website-React-/static/media/logo.c7c76c7a.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/Resort-management-website-React-/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/Resort-management-website-React-/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/Resort-management-website-React-/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "0e293352532d9483616fe4c5110c8e28",
+    "url": "/Resort-management-website-React-/static/media/room-12.0e293352.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/Resort-management-website-React-/static/media/room-2.052c7681.jpeg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/Resort-management-website-React-/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "1e407d88a352a0e8493e2275a76f39f5",
+    "url": "/Resort-management-website-React-/static/media/room-4.1e407d88.jpeg"
+  },
+  {
+    "revision": "a5c06e0bb3e90f927159439d68b07253",
+    "url": "/Resort-management-website-React-/static/media/room-5.a5c06e0b.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/Resort-management-website-React-/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/Resort-management-website-React-/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "ebb7fca5f456fb07e0096ab3ef72b330",
+    "url": "/Resort-management-website-React-/static/media/room-8.ebb7fca5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/Resort-management-website-React-/static/media/room-9.7b17c49f.jpeg"
+  }
+]);
